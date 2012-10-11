5b59ae0eca6752336c928e0e7e71bd41fb4cf9ce
John Brinkman <john@brinko.com>
2012-10-10 22:25:45 -0700

    inital project setup

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject scary-sound-machine "0.1.0-SNAPSHOT"
+  :description "Using Clojure and the overtone project, download and play scary sounds from freesound.org"
+  :url "https://github.com/luntar/scary-sound-machine"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [overtone "0.7.1"]])
---
+++ test/scary_sound_machine/core_test.clj
@@ -0,0 +1,7 @@
+(ns scary-sound-machine.core-test
+  (:use clojure.test
+        scary-sound-machine.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
